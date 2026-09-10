52388fefbfc5840b2ff552688e187ed41cb7c624
ethereal86 <colinyeo86@gmail.com>
2026-09-10 18:27:45 +0700

    'Hello, world!' program and basic build system

---
+++ src/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello, world!" << std::endl;
+    std::cout << "This is a project made by Colin!" << std::endl;
+    return 0;
+}
